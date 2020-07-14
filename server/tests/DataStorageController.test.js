17a8e1369d8cab4419708a2e0314f7b77bb1a489
William Goodwin <williamgoodwin@Williams-MacBook-Pro.local>
2020-07-14 18:34:54 -0400

    Testing for the client and server are set up and can be run with npm test. Also created the client controller files.

---
+++ server/tests/DataStorageController.test.js
@@ -0,0 +1,25 @@
+beforeAll(() => {
+    //return initializeDatabase();
+});
+
+afterAll(() => {
+    //return clearDatabase();
+});
+
+test('SQL dump importer', () => {});
+
+test('SQL dump exporter', () => {});
+
+test('get game questions', () => {});
+
+test('get game categories', () => {});
+
+test('read a question', () => {});
+
+test('update category name', () => {});
+
+test('update category color', () => {});
+
+test('add new question', () => {});
+
+test('delete question', () => {});
