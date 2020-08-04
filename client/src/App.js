79262ff391ec921040f94e5dce905417284e8794
Miaolin Jia <miaolin.jia@viasat.com>
2020-08-03 21:42:43 -0400

    Changes for the Help page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Admin from './pages/Admin/Admin'
-import Questions from './pages/Admin/Questions'
-import NewQuestion from './pages/Admin/NewQuestion'
-import EditQuestion from './pages/Admin/EditQuestion'
-import Game from './pages/Game/Game'
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin/Admin";
+import Questions from "./pages/Admin/Questions";
+import NewQuestion from "./pages/Admin/NewQuestion";
+import EditQuestion from "./pages/Admin/EditQuestion";
+import Game from "./pages/Game/Game";
+import Help from "./pages/Help/Help";
 
 function App() {
   return (
@@ -22,7 +23,10 @@ function App() {
               <Route path={`${url}/`} component={Admin} exact />
               <Route path={`${url}/questions`} component={Questions} exact />
               <Route path={`${url}/questions/new`} component={NewQuestion} />
-              <Route path={`${url}/questions/edit/:id`} component={EditQuestion} />
+              <Route
+                path={`${url}/questions/edit/:id`}
+                component={EditQuestion}
+              />
             </>
           )}
         />
@@ -34,9 +38,17 @@ function App() {
             </>
           )}
         />
+        <Route
+          path="/help"
+          render={({ match: { url } }) => (
+            <>
+              <Route path={`${url}/`} component={Help} exact />
+            </>
+          )}
+        />
       </Switch>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
