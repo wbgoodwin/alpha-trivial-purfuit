dabdc78a79f5994730b45f3deb32dc19b7c0ea78
Alyson Givre <alysong24@gmail.com>
2020-08-12 18:03:33 -0400

    Randomized the order in which answers to questions appear

--- client/src/components/GameBoard/GameQuestions.js
+++ client/src/components/GameBoard/GameQuestions.js
@@ -1,120 +1,185 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
-    CardContent, CardHeader, Grid, Button, TextField, Paper, Select, MenuItem,
+    CardContent, CardHeader, Grid, Button, Paper, Select, MenuItem,
     InputLabel, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio
   } from '@material-ui/core'
 import { retrieveQuestionAnswersSet } from '../../controllers/GameLogicController'
 
 const GameQuestions = (props) => {
     const [category, setCategory] = useState("")
-    const [question, setQuestion] = useState({})
+    const [question, setQuestion] = useState(undefined)
+    const [shuffledAnswers, setShuffledAnswers] = useState([])
     const [usersAnswer, setUsersAnswer] = useState("")
     const [answerIsCorrect, setAnswerIsCorrect] = useState(false)
     const [questionAnswered, setQuestionAnswered] = useState(false)
 
+    useEffect(() => {
+      if (question) {
+        let answers = [question.correct_answer, question.incorrect_answer1,
+                       question.incorrect_answer2, question.incorrect_answer3]
+        setShuffledAnswers(shuffle(answers))
+      }
+    }, [question])
+
+    const shuffle = (a) => {
+        var j, x, i
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1))
+            x = a[i]
+            a[i] = a[j]
+            a[j] = x
+        }
+        return a
+    }
 
     const getQuestionToShow = async (categoryName) => {
-        let questionAnswersSet = await retrieveQuestionAnswersSet(categoryName);
-        setQuestion(questionAnswersSet);
-        setQuestionAnswered(false)
-        setUsersAnswer("")
+      let questionAnswersSet = await retrieveQuestionAnswersSet(categoryName);
+      setQuestion(questionAnswersSet);
+      setQuestionAnswered(false)
+      setUsersAnswer("")
     }
 
     const checkAnswer = () => {
-        if (question.correct_answer === usersAnswer) {
-            setAnswerIsCorrect(true)
-        }
-        else {
-            setAnswerIsCorrect(false)
-        }
-        setQuestionAnswered(true)
+      if (question.correct_answer === usersAnswer) {
+        setAnswerIsCorrect(true)
+      }
+      else {
+        setAnswerIsCorrect(false)
+      }
+      setQuestionAnswered(true)
     }
 
     const userInstructions = () => {
-        if (answerIsCorrect) {
-            return<div>Correct. Roll Again!</div>
-        }
-        return<div>Incorrect. Next Player's Turn</div>
+      return (
+        <Grid container direction="row">
+          <Grid item style={{'marginTop': '5px'}}>
+            <strong>
+              {answerIsCorrect ?
+                "Correct. Roll Again!" :
+                "Incorrect. Next Player's Turn"
+              }
+            </strong>
+          </Grid>
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={resetState}
+            style={{
+              'marginLeft': '10px'
+            }}
+          >
+            New Question
+          </Button>
+        </Grid>
+      )
     }
 
+    const resetState = () => {
+      setCategory("")
+      setShuffledAnswers([])
+      setQuestion(undefined)
+      setUsersAnswer("")
+      setAnswerIsCorrect(false)
+      setQuestionAnswered(false)
+    }
+
+    const mapCategories = (category, index) => {
+      return (
+        <MenuItem key={index} value={category.getName()}>
+          {`${category.getName()} (${category.getColorName()})`}
+        </MenuItem>
+      )
+    }
+
+    const renderAnswers = () => {
+      return (
+        <Grid item style={{'marginTop': '10px'}}>
+          <FormControl component="fieldset">
+          <FormLabel component="legend">{question.question}</FormLabel>
+              <RadioGroup
+                aria-label="answers"
+                name="answers" value={usersAnswer}
+                onChange={(event) => setUsersAnswer(event.target.value)}
+              >
+                {shuffledAnswers.map(mapAnswers)}
+              </RadioGroup>
+          </FormControl>
+        </Grid>
+      )
+    }
+
+    const mapAnswers = (answer, index) => {
+      return (
+        <FormControlLabel
+          key={index}
+          value={answer}
+          control={<Radio />}
+          label={answer}
+        />
+      )
+    }
 
     return (
-        <div>
-            <Grid item xs={11} style={{'marginBottom': '10px'}}>
-                <Paper>
-                <CardHeader title="Game Questions"/>
-                <CardContent>
-                    <Grid direction="column" container>
-                    <Grid item>
-                        <FormControl>
-                        <InputLabel htmlFor="category-select">Select Category</InputLabel>
-                        <Select
-                            id="category-select"
-                            name="category"
-                            value={category}
-                            label="Select Category"
-                            onChange={(e) => setCategory(e.target.value)}
-                            style={{'minWidth': '20vw'}}
-                        >
-                            {props.categories === undefined ? null : props.categories.map((cat, index) =>
-                                <MenuItem key={index} value={cat.getName()}>
-                                {cat.getName() + " (" + cat.getColorName() + ")"}
-                                </MenuItem>
-                                )}
-                        </Select>
-                        </FormControl>
-                                &nbsp;
-                                &nbsp;
+      <Grid item xs={11} style={{'marginBottom': '10px', 'maxWidth': '100%'}}>
+        <Paper>
+          <CardHeader title="Game Questions"/>
+          <CardContent>
+            <Grid direction="row" container>
+
+              {!question ?
+                <Grid item>
+                  <FormControl>
+                    <InputLabel htmlFor="category-select">Select Category</InputLabel>
+                    <Select
+                        id="category-select"
+                        name="category"
+                        value={category}
+                        label="Select Category"
+                        onChange={(e) => setCategory(e.target.value)}
+                        style={{'minWidth': '20vw'}}
+                    >
+                      {props.categories.map(mapCategories)}
+                    </Select>
+                  </FormControl>
+                  <Button
+                    disabled={category === ""}
+                    color="primary"
+                    variant="contained"
+                    onClick={() => getQuestionToShow(category)}
+                    style={{
+                      'marginLeft': '10px',
+                      'marginTop': '12px'
+                    }}
+                  >
+                      Get New Question
+                  </Button>
+                </Grid>
+                :
+                renderAnswers()
+              }
+
+              {!question ? null :
+                <Grid container direction="column">
+                  <Grid item>
+                    {(question && questionAnswered) ?
+                        userInstructions()
+                        :
                         <Button
-                            disabled={category === ""}
+                            disabled={usersAnswer === ""}
                             color="primary"
                             variant="contained"
-                            onClick={() => getQuestionToShow(category)}
-                            >
-                            Get New Question
+                            onClick={checkAnswer}
+                        >
+                            Check Answer
                         </Button>
-                    </Grid>
-
-                    {Object.keys(question).length === 0 && question.constructor === Object ?
-                        null
-                    :
-                        <div>
-                            <br/>
-                            <Grid item>
-                                <FormControl component="fieldset">
-                                <FormLabel component="legend">{question.question}</FormLabel>
-                                    <RadioGroup aria-label="answers" name="answers" value={usersAnswer} onChange={(event) => setUsersAnswer(event.target.value)}>
-                                        <FormControlLabel value={question.correct_answer} control={<Radio />} label={question.correct_answer} />
-                                        <FormControlLabel value={question.incorrect_answer1} control={<Radio />} label={question.incorrect_answer1} />
-                                        <FormControlLabel value={question.incorrect_answer2} control={<Radio />} label={question.incorrect_answer2} />
-                                        <FormControlLabel value={question.incorrect_answer3} control={<Radio />} label={question.incorrect_answer3} />
-                                    </RadioGroup>
-                                </FormControl>
-                            </Grid>
-                            <div>
-                                {questionAnswered ?
-                                    userInstructions()
-                                 :
-                                <Button
-                                    disabled={usersAnswer === ""}
-                                    color="primary"
-                                    variant="contained"
-                                    onClick={() => checkAnswer()}
-                                >
-                                    Check Answer
-                                </Button>
-                                }
-
-                            </div>
-
-                        </div>
                     }
-
-                    </Grid>
-                </CardContent>
-                </ Paper>
+                  </Grid>
+                </Grid>
+              }
             </Grid>
-        </div>
+          </CardContent>
+        </Paper>
+      </Grid>
     )
 }
 
